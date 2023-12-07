ca050d283e897ef8470b75505439028babc63d62
Baetato <134314347+Baetato@users.noreply.github.com>
2023-12-07 12:37:09 +0900

    week09/jerry

--- week09/jerry/src/App.js
+++ week09/jerry/src/App.js
@@ -1,57 +1,53 @@
-import { useEffect, useMemo, useState } from "react";
-import axios from "axios";
+import "./App.css";
+import { useCookies } from "react-cookie";
+import React, { useEffect, useState } from "react";
 
-const API_KEY = "08a2262588600c40fa365d59b052d542";
+function App() {
+  const [text, setText] = useState("");
+  const [isRemember, setIsRemember] = useState(false);
+  const [cookies, setCookie, removeCookie] = useCookies(["rememberText"]);
+  //cookies는 쿠키의 정의,
+  //setCookie는 쿠키를 재정의,
+  //removeCookie는 쿠키 제거이다.
+  //useCookies 안에는 초기값을 넣는다.
 
-const App = () => {
-  const [city, setCity] = useState("");
-  const [weather, setWeather] = useState(null);
+  let now = new Date();
+  let after1m = new Date();
 
-  const getDatas = async () => {
-    try {
-      const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-      );
+  useEffect(() => {
+    if (cookies.rememberText !== undefined) {
+      setText(cookies.rememberText);
+      setIsRemember(true);
+    }
+  });
 
-      setWeather(data);
-    } catch (e) {
-      setWeather(null);
-      console.error(e);
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  const handleOnChange = (e) => {
+    after1m.setMinutes(now.getMinutes() + 1); // after1m를 현재시간의 1분뒤로 정의
+    setIsRemember(e.target.checked);
+    if (e.target.checked) {
+      setCookie("remeberText", text, { path: "/", expires: after1m });
+      // remeberText에 text라는 값을 넣는다.
+      // path는 적용되는 도메인
+      // expires = 만료시간은 1분뒤
+    } else {
+      removeCookie("rememberText");
+      // checkBox의 체크를 지울시
+      // 쿠키 remeberText를 지운다
     }
   };
 
-  const temparature = useMemo(() => {
-    if (!weather) return null;
-    return Math.round((weather.main.temp - 273.15) * 10) / 10;
-  }, [weather]);
-
-  useEffect(() => {
-    if (city !== "") {
-      getDatas();
-    }
-  }, [city]);
-
   return (
     <>
-      <input
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        placeholder="도시를 입력하세요."
-      />
-
-      {weather && (
-        <div className="weather">
-          <div>
-            <span className="city">{weather.name}</span>
-            <span className="temparature">{temparature}°C</span>
-          </div>
-          {weather.weather && (
-            <span className="main">{weather.weather[0].main}</span>
-          )}
-        </div>
-      )}
+      {" "}
+      <input value={text} onChange={onChange} />{" "}
+      <input type="checkBox" onChange={handleOnChange} checked={isRemember} />{" "}
+      <h1>{text}</h1>{" "}
     </>
   );
-};
+}
 
 export default App;
--- week09/jerry/src/index.js
+++ week09/jerry/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-
+import ReactDOM from "react-dom";
 import App from "./App";
+import { CookieProvider } from "react-cookie";
 
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <CookieProvider>
     <App />
-  </React.StrictMode>
+  </CookieProvider>,
+  document.getElementById("root")
 );
