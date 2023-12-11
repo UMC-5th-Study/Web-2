4bc709dd179f432f5e56795932ec074464b15c0b
Baetato <134314347+Baetato@users.noreply.github.com>
2023-12-11 10:29:47 +0900

    week10/jerry

---
+++ week10/jerry/src/App.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState({ id: "", pw: "" });
+
+  const handleChangeInput = ({ target: { name, value } }) => {
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (data.id === "" || data.pw === "") {
+      alert("값이 비어있습니다.");
+      return;
+    }
+
+    try {
+      const { result } = (
+        await axios.post("http://localhost:8000/user/login", data)
+      ).data;
+
+      alert("로그인 성공");
+
+      localStorage["id"] = result.userId;
+      localStorage["token"] = result.AccessToken;
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="id"
+        value={data.id}
+        placeholder="아이디를 입력해주세요."
+        onChange={handleChangeInput}
+      />
+
+      <input
+        type="password"
+        name="pw"
+        value={data.pw}
+        placeholder="비밀번호를 입력해주세요."
+        onChange={handleChangeInput}
+      />
+
+      <button type="submit">로그인</button>
+    </form>
+  );
+};
+
+export default App;
